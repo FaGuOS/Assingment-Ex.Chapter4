b4a4a010bd6c2b453950471fd6d22322db9c46cd
EC2 Default User <ec2-user@ip-172-31-42-87.ap-northeast-3.compute.internal>
2024-04-05 18:58:50 +0000

    Assingment EX4 Commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :book_comments, only: [:create, :destroy]
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+  end
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
